fae4e1434af4de0796dff2ad205fa003727cbe66
SurinJeon <surin0901@naver.com>
2021-03-11 21:13:55 +0900

    add new files

--- src/main/java/swing_study/dlg/JFileChooserEx.java
+++ src/main/java/swing_study/dlg/JFileChooserEx.java
@@ -1,61 +1,111 @@
 package swing_study.dlg;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 @SuppressWarnings("serial")
 public class JFileChooserEx extends JFrame implements ActionListener {
-
 	private JPanel contentPane;
-	private JButton btn1;
+	private JButton btnOpen;
+	private JLabel lblImg;
+	private JFileChooser chooser = new JFileChooser(System.getProperty("user.dir"));
+	FileInputStream fis;
 
 	public JFileChooserEx() {
 		initialize();
 	}
+
 	private void initialize() {
 		setTitle("JFileChooser Ex");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 429, 170);
+		setBounds(100, 100, 457, 388);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		JLabel lbl1 = new JLabel("New label");
-		contentPane.add(lbl1, BorderLayout.CENTER);
-		
-		btn1 = new JButton("클릭");
-		btn1.addActionListener(this);
-		contentPane.add(btn1, BorderLayout.EAST);
+
+		lblImg = new JLabel("");
+		contentPane.add(lblImg, BorderLayout.CENTER);
+
+		btnOpen = new JButton("이미지 불러오기");
+		btnOpen.addActionListener(this);
+		contentPane.add(btnOpen, BorderLayout.SOUTH);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == btn1) {
-//			actionPerformedBtn1(e);
+		try {
+			if (e.getSource() == btnOpen) {
+				if (e.getActionCommand().equals("이미지 불러오기")) {
+
+					actionPerformedBtnOpen(e);
+
+				} else {
+					actionPerformedBtnSave(e);
+				}
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
 		}
 	}
-//	protected void actionPerformedBtn1(ActionEvent e) {
-//	 JOptionPane.showmessage
-//	}
+
+	private void actionPerformedBtnSave(ActionEvent e) {
+		// download 폴더에 lbl에 뜬 사진을 저장되도록 만들기
+		// 해당 폴더가 없다면 새성 후 저장되게 만들기
+		int res = chooser.showSaveDialog(null);
+		if (res != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(null, "파일을 선택하지 않았습니다.", "WARNING", JOptionPane.WARNING_MESSAGE);
+			return;
+		} else {
+
+			String chooseFilePath = chooser.getSelectedFile().getPath();
+
+			int data;
+
+			try (FileOutputStream fos = new FileOutputStream(chooseFilePath)) {
+				while ((data = fis.read()) != -1) {
+					fos.write(data);
+				}
+
+			} catch (FileNotFoundException e3) {
+				e3.printStackTrace();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+
+			}
+		}
+	}
+
+	protected void actionPerformedBtnOpen(ActionEvent e) throws FileNotFoundException {
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("JPG & PNG images", "jpg", "png", "gif");
+		chooser.setFileFilter(filter);
+		int res = chooser.showOpenDialog(null);
+		if (res != JFileChooser.APPROVE_OPTION) {
+			JOptionPane.showMessageDialog(null, "파일을 선택하지 않았습니다.", "WARNING", JOptionPane.WARNING_MESSAGE);
+			return;
+		} // 여기 넘어오면 approve option 선택했다는 뜻
+
+		String chooseFilePath = chooser.getSelectedFile().getPath();
+		lblImg.setIcon(new ImageIcon(chooseFilePath));
+
+		if (btnOpen.getText().equals("이미지 불러오기")) {
+			btnOpen.setText("이미지 저장하기");
+		}
+
+		fis = new FileInputStream(chooseFilePath);
+
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
